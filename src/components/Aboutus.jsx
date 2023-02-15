4788459a21f4c4afb774034ce5afdec1afa6f0d4
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-02-15 09:31:40 +0000

    completing about page

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,78 +1,99 @@
-import React from 'react'
+import React from "react";
 
 const Aboutus = () => {
   return (
     <div>
-      <section class="text-gray-300 body-font ">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <img
-            class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-            alt=""
-            src="assets/our.svg"
-          />
-          <div class="text-center lg:w-2/3 w-full ">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300">
-              Our Story
-            </h1>
-            <p class="mb-8 leading-relaxed text-lg py-2">
-              At Zap Technologies, we're passionate about using digital
-              innovation to transform businesses and drive growth. Our agency
-              was founded in 2022 with a mission to provide a more
-              customer-centric approach to digital marketing and design.
-            </p>
-            <p class="mb-8 leading-relaxed text-lg py-2">
-              As we grew, we saw firsthand the impact that quality software
-              development can have on a business's success. That's why we've
-              made it a priority to not only deliver top-notch development
-              services, but also to train and mentor the next generation of
-              software developers. Our team of experts is comprised of
-              experienced software developers who are dedicated to staying on
-              the cutting edge of the latest technologies and best practices.
-              But we don't just keep that knowledge to ourselves - we believe in
-              sharing our expertise and helping others grow in their skills and
-              careers. Through our training and mentoring programs, we've helped
-              countless individuals develop the skills and confidence they need
-              to succeed in the software development industry.
-            </p>
-            <p class="mb-8 leading-relaxed text-lg py-2">
-              From coding bootcamps to one-on-one coaching, we offer a range of
-              programs designed to meet the needs of individuals at any stage of
-              their career. At ZapTek, we believe that investing in our people
-              is the key to our success. By providing ongoing training and
-              mentoring, we ensure that our team is always equipped with the
-              skills and knowledge they need to deliver the best possible
-              results for our clients. So whether you're looking for top-quality
-              software development services or seeking to grow your skills as a
-              developer, we're here to help. Let's work together to create a
-              brighter future for your business and your career.
-            </p>
-            {/* At Zap Technologies, we believe in the power of digital innovation
-              to transform businesses and drive growth. Founded in 2022, our
-              agency was born out of a desire to provide a more customer-centric
-              approach to digital marketing and design. From the beginning, our
-              vision has been to create a world where every business has the
-              opportunity to thrive online. We know that in today's fast-paced
-              digital landscape, it's not enough to simply have a website or
-              social media presence. To truly succeed, businesses need to be
-              agile, innovative, and constantly evolving to meet the needs of
-              their customers. That's where we come in. Our team of experts is
-              passionate about using the latest technologies and best practices
-              to help our clients stand out in a crowded marketplace. Whether
-              it's through stunning visual design, targeted advertising
-              campaigns, or data-driven insights, we work tirelessly to help our
-              clients achieve their business goals and grow their bottom line.
-              At Zaptek, we believe that every client is unique, and we take a
-              customized approach to every project we undertake. We listen
-              closely to our clients' needs and work collaboratively to develop
-              solutions that are tailored to their specific goals and
-              challenges. So whether you're a small business just starting out
-              or a large enterprise looking to take your digital presence to the
-              next level, we're here to help. Let's work together to create a
-              brighter future for your business in the digital age.
-           */}
+      <section class="text-gray-300 text-lg body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300">
+                Our Story
+              </h1>
+
+              <p class="leading-relaxed mb-4">
+                At Zap Technologies, we're passionate about using digital
+                innovation to transform businesses and drive growth. Our agency
+                was founded in 2022 with a mission to provide a more
+                customer-centric approach to digital marketing and design.
+              </p>
+              <p class="mb-8 leading-relaxed text-lg py-2">
+                At Zap Technologies, we're passionate about using digital
+                innovation to transform businesses and drive growth. Our agency
+                was founded in 2022 with a mission to provide a more
+                customer-centric approach to digital marketing and design.
+              </p>
+            </div>
+            <img
+              alt="ecommerce"
+              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="assets/our.svg"
+            />
           </div>
         </div>
       </section>
+
+      <section class="text-gray-300 text-lg body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <img
+              alt="ecommerce"
+              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="assets/inn.svg"
+            />
+            <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300">
+                Quality Being A Priority
+              </h1>
+              <p class="leading-relaxed">
+                As we grew, we saw firsthand the impact that quality software
+                development can have on a business's success. That's why we've
+                made it a priority to not only deliver top-notch development
+                services, but also to train and mentor the next generation of
+                software developers. Our team of experts is comprised of
+                experienced software developers who are dedicated to staying on
+                the cutting edge of the latest technologies and best practices.
+                But we don't just keep that knowledge to ourselves - we believe
+                in sharing our expertise and helping others grow in their skills
+                and careers. Through our training and mentoring programs, we've
+                helped countless individuals develop the skills and confidence
+                they need to succeed in the software development industry.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="text-gray-300 text-lg body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300">
+                Our Support Towards Community
+              </h1>
+
+              <p class="leading-relaxed mb-4">
+                From coding bootcamps to one-on-one coaching, we offer a range
+                of programs designed to meet the needs of individuals at any
+                stage of their career. At ZapTek, we believe that investing in
+                our people is the key to our success. By providing ongoing
+                training and mentoring, we ensure that our team is always
+                equipped with the skills and knowledge they need to deliver the
+                best possible results for our clients. So whether you're looking
+                for top-quality software development services or seeking to grow
+                your skills as a developer, we're here to help. Let's work
+                together to create a brighter future for your business and your
+                career.
+              </p>
+            </div>
+            <img
+              alt="ecommerce"
+              class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src="assets/ed.svg"
+            />
+          </div>
+        </div>
+      </section>
+
       <section class="text-white body-font">
         <div>
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300 text-center">
@@ -124,6 +145,37 @@ const Aboutus = () => {
       </section>
     </div>
   );
-}
+};
 
-export default Aboutus
+export default Aboutus;
+
+
+
+  {
+    /* At Zap Technologies, we believe in the power of digital innovation
+              to transform businesses and drive growth. Founded in 2022, our
+              agency was born out of a desire to provide a more customer-centric
+              approach to digital marketing and design. From the beginning, our
+              vision has been to create a world where every business has the
+              opportunity to thrive online. We know that in today's fast-paced
+              digital landscape, it's not enough to simply have a website or
+              social media presence. To truly succeed, businesses need to be
+              agile, innovative, and constantly evolving to meet the needs of
+              their customers. That's where we come in. Our team of experts is
+              passionate about using the latest technologies and best practices
+              to help our clients stand out in a crowded marketplace. Whether
+              it's through stunning visual design, targeted advertising
+              campaigns, or data-driven insights, we work tirelessly to help our
+              clients achieve their business goals and grow their bottom line.
+              At Zaptek, we believe that every client is unique, and we take a
+              customized approach to every project we undertake. We listen
+              closely to our clients' needs and work collaboratively to develop
+              solutions that are tailored to their specific goals and
+              challenges. So whether you're a small business just starting out
+              or a large enterprise looking to take your digital presence to the
+              next level, we're here to help. Let's work together to create a
+              brighter future for your business in the digital age.
+           */
+  }
+
+   
