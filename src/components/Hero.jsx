0fcd38f060d4d8775fb95a033045986e4157125d
Samuel Agyemang <89325057+Sam-Tech-Ideas@users.noreply.github.com>
2023-02-28 22:22:46 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 
 const Hero = () => {
@@ -21,7 +21,9 @@ const Hero = () => {
           </p>
           <div className="flex justify-center  ">
             <button className=" text-white bg-blue-600 border-0 py-2 px-3  hover:bg-blue-700 rounded text-lg">
+    <Link to={"/services"}>
               Our Services
+    </Link>
             </button>
           </div>
         </div>
