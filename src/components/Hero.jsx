07cdb01290e28ac68304538d38f811e088d83129
Samuel Agyemang <89325057+Sam-Tech-Ideas@users.noreply.github.com>
2023-06-13 11:25:32 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,12 +20,13 @@ const Hero = () => {
             cutting-edge technologies and innovative strategies.
           </p>
           <div className="flex justify-center  ">
-
+     <Link to={"/services"}>
             <button className=" text-white bg-blue-600 border-0 py-2 px-3  hover:bg-blue-700 rounded text-lg">
-        <Link to={"/services"}>
+   
               Our Services
-    </Link>
+
             </button>
+    </Link>
 
           </div>
         </div>
